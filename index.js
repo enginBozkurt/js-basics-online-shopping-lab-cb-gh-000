0823197f9571887665aebc9194fa7c0f5c09da9e
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-15 14:51:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,36 +40,30 @@ else if (cart.length===1){
  results.push(`${item} at $${price}`);
 
   }
-  
+
   // 'In your cart, you have mango at $84 and nuts at $29.' ]
  console.log(`In your cart, you have ${results.join(' and ')}.`);
- } 
+ }
   else {
- 
+
    for(var j =0; j<cart.length; j++){
   item = Object.keys(cart[j]) ;
-  price = Object.values(cart[j]); 
+  price = Object.values(cart[j]);
   var last = j===cart.length-1
   var lastPrice = j===cart.length-1
-  console.log(last) 
+  console.log(last)
   if ((!last) && (!lastPrice)){
-  
+
  results.push(`${item} at $${price}`)
 
 
   }
 else {
   results.push(`and ${item} at $${price}`)
-  
 }
-
- 
 }
- console.log(`In your cart, you have ${results.join(', ')}.`)    
+ console.log(`In your cart, you have ${results.join(', ')}.`)
   }
-
-
-
 }
 
 // The total() function accepts no arguments, iterates through the cart array,
